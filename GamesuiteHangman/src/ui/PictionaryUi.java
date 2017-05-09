735789b5b033fd5d65e200e7287079b6ca3b071b
LAPTOP-GMI2185F\Bert Leysen <bert.leysen@ucll.be>
2017-05-09 15:27:34 +0200

    Story 3.6 Aanpassingen Driehoek en UI

--- GamesuiteHangman/src/ui/PictionaryUi.java
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -17,7 +17,7 @@ public class PictionaryUi {
 		Punt p = new Punt(x,y);
 		JOptionPane.showConfirmDialog(null,"U heeft een correct punt aangemaakt: " + p.toString());
 		
-		Object[] shapes = { "Cirkel", "Rechthoek"};
+		Object[] shapes = { "Cirkel", "Rechthoek", "Driehoek"};
 
 		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen?", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
 		
@@ -30,6 +30,22 @@ public class PictionaryUi {
 			int hoogte = Integer.parseInt(JOptionPane.showInputDialog("Hoogte van de rechthoek: "));
 			Rechthoek rechthoek = new Rechthoek(p, breedte, hoogte);
 			JOptionPane.showConfirmDialog(null, "Uw heeft een correcte rechthoek aangemaakt: " + rechthoek);
+		} else if(keuze.equals("Driehoek")) {
+			int x2 = Integer.parseInt(JOptionPane.showInputDialog("X coördinaat van het tweede punt: "));
+			int y2 = Integer.parseInt(JOptionPane.showInputDialog("Y coördinaat van het tweede punt: "));
+			
+			Punt p2 = new Punt(x2,y2);
+			JOptionPane.showConfirmDialog(null,"U heeft een correct punt aangemaakt: " + p2.toString());
+			
+			int x3 = Integer.parseInt(JOptionPane.showInputDialog("X coördinaat van het derde punt: "));
+			int y3 = Integer.parseInt(JOptionPane.showInputDialog("Y coördinaat van het derde punt: "));
+			
+			Punt p3 = new Punt(x3,y3);
+			JOptionPane.showConfirmDialog(null,"U heeft een correct punt aangemaakt: " + p3.toString());
+			
+			Driehoek d = new Driehoek(p,p2,p3);
+			JOptionPane.showConfirmDialog(null, "Uw heeft een correcte driehoek aangemaakt: " + d);
+			
 		}
 		
 		
