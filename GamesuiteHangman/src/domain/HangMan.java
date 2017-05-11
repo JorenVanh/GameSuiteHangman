9297f0e5ad9921d2c92b282efe45815b58c2f00f
asmi_adh <asmita.adhikari@student.ucll.be>
2017-05-11 13:53:20 +0200

    Hangman

---
+++ GamesuiteHangman/src/domain/HangMan.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class HangMan {
+
+	public Object getSpeler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getHint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getNaam() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Tekening getTekening() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- GamesuiteHangman/src/ui/HangManHoofdScherm.java
+++ GamesuiteHangman/src/ui/HangManHoofdScherm.java
@@ -9,7 +9,7 @@ public class HangManHoofdScherm extends GameHoofdScherm {
 	private static final long serialVersionUID = 1L;
 	
 	public HangManHoofdScherm(HangMan spel, HangmanPaneel spelpaneel){
-		super("Hangman - "+spel.getSpeler().getNaam(), spel.getTekening());	
+		super("Hangman - "+((HangMan) spel.getSpeler()).getNaam(), spel.getTekening());	
 		
 		spelpaneel.setTekenVenster(getTekenvenster());
 		this.add(spelpaneel, BorderLayout.SOUTH);
